feca5285b126b2c9a2ebc5cb165cba4214fdb9f6
Md Abidur Rahman <abid19918february@gmail.com>
2025-08-25 11:40:43 +0600

    Withdraw Feature is completed

--- home.js
+++ home.js
@@ -1,5 +1,6 @@
 const validPin = 1234
-
+const withdrawPassword = 4321
+// Add Money Features Javascript 
 document.getElementById("add-money-btn").addEventListener("click",function(e){
     e.preventDefault()
     // console.log("add-money-btn-clicked")
@@ -25,7 +26,29 @@ document.getElementById("add-money-btn").addEventListener("click",function(e){
 
     document.getElementById("available-balance").innerText = newBalance
 })
+// Cash-out Money Features Javascript 
+ document.getElementById("withdraw-btn").addEventListener("click",function(e){
+    e.preventDefault()
 
+    const withdrawAccountNumber = document.getElementById("withdraw-account").value
+    const withdrawAmount = parseInt(document.getElementById("withdraw-amount").value)
+    const withdrawPin = parseInt(document.getElementById("withdraw-pin").value)
+
+    if(withdrawAccountNumber.length < 11){
+        alert("Please Provide a Valid Account Number")
+        return;
+    }
+    if(withdrawPin !== withdrawPassword){
+        alert("Please Provide Valid PIN Number")
+        return;
+    }
+
+    const withdrawBalance = parseInt(document.getElementById("available-balance").innerText)
+    const withdrawNewBalance = withdrawBalance - withdrawAmount
+
+    document.getElementById("available-balance").innerText = withdrawNewBalance
+    
+ })
 // toggling feature 
 document.getElementById("add-button").addEventListener("click",function(){
     document.getElementById("cash-out-parent").style.display = "none"
